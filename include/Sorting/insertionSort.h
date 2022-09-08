97375b0603a0f9299e4e952d806d6a6a664cdd53
Dera Uchenwoke <uchidera1@gmail.com>
2022-09-08 19:14:37 +0100

    updated diffAlgos and now understand insertionSort in ascending order

---
+++ include/Sorting/insertionSort.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+
+void insertSort()
+{
+	const int size = 6;
+	int arr[size] = { 31, 41, 59, 26, 41, 58 };
+
+	for (int i = 1; i < size; ++i)
+	{
+		int key = arr[i]; 
+	
+		int j = i - 1;
+
+		while (j >= 0 && arr[j] > key) 
+		{
+			arr[j + 1] = arr[j]; // shifts value 'upwards', arr[j] to the next index 
+
+			j = j - 1;
+			
+		}
+
+		// std::cout << j + 1 << "\n"; 
+
+		arr[j + 1] = key; // inserts value of key into +1 the position the while loop was terminated...
+		// a.k.a when the condition, arr[j] > key is no longer met.
+	}
+
+	for (int x : arr)
+	{
+		std::cout << x << "\n"; 
+	}
+}
--- src/diffAlgos.cpp
+++ src/diffAlgos.cpp
@@ -5,23 +5,26 @@
 //#include "../include/Miscellaneous/bdayCandles.h"
 //#include "../include/Miscellaneous/timeConversion.h"
 //#include "../include/Miscellaneous/attributeParser.h"
-#include "../include/Miscellaneous/minMaxSum.h"
+//#include "../include/Miscellaneous/minMaxSum.h"
+#include "../include/Sorting/insertionSort.h"
 
+# include <vector>
+# include <iostream>
 
 int main() {
 
-    std::vector<int> arr;
-    long long int arrLength = 0; 
+    //std::vector<int> arr;
+    //long long int arrLength = 0; 
 
-    std::cin >> arrLength; 
+    //std::cin >> arrLength; 
 
-    for (int val = 0; val < arrLength; val++) {
-        int userInput = 0; 
-        std::cin >> userInput;
-        arr.push_back(userInput);
-    }
+    //for (int val = 0; val < arrLength; val++) {
+    //    int userInput = 0; 
+    //    std::cin >> userInput;
+    //    arr.push_back(userInput);
+    //}
 
-    minMaxSum(arr);
+    insertSort();
 
     return 0;
 }
